0ee7b794644f564e4f9a49c1f0a59495a5636a7e
dead-beef <dead-beef@users.noreply.github.com>
2020-07-30 07:36:36 +0000

    refactor(form): refactor range inputs

--- apps/webaudio-spectrum-analyser/src/app/components/input-frequency/input-frequency.component.ts
+++ apps/webaudio-spectrum-analyser/src/app/components/input-frequency/input-frequency.component.ts
@@ -1,16 +1,11 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  forwardRef,
-  Input,
-  OnChanges,
-  SimpleChanges,
-} from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+import { InputRangeComponent } from '../input-range/input-range.component';
 
 @Component({
   selector: 'app-input-frequency',
-  templateUrl: './input-frequency.component.html',
+  templateUrl: '../input-range/input-range.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [
     {
@@ -20,94 +15,17 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
     },
   ],
 })
-export class InputFrequencyComponent
-  implements ControlValueAccessor, OnChanges {
-  @Input() public name: string;
-
-  @Input() public min = 20;
-
-  @Input() public max = 20000;
-
-  public log = 0;
-
-  public logMin = Math.log2(this.min);
-
-  public logMax = Math.log2(this.max);
-
-  public logName = '';
-
-  public _value = 0;
-
+export class InputFrequencyComponent extends InputRangeComponent {
   /**
-   * Value getter.
+   * Constructor.
    */
-  public get value(): number {
-    return this._value;
-  }
-
-  /**
-   * Value setter.
-   */
-  public set value(v: number) {
-    this._value = v;
-    this.onChange(this._value);
-  }
-
-  private onChange = (_: any) => {};
-
-  /**
-   * Lifecycle hook.
-   * @param changes
-   */
-  public ngOnChanges(changes: SimpleChanges): void {
-    //console.log(changes);
-    if (changes.min) {
-      this.logMin = Math.log2(changes.min.currentValue);
-    }
-    if (changes.max) {
-      this.logMax = Math.log2(changes.max.currentValue);
-    }
-    if (changes.name) {
-      this.logName = `log${changes.name.currentValue}`;
-    }
-  }
-
-  /**
-   * ControlValueAccessor
-   * @param value
-   */
-  public writeValue(value: any): void {
-    this.setValue(Number(value));
-  }
-
-  /**
-   * ControlValueAccessor
-   * @param fn
-   */
-  public registerOnChange(fn): void {
-    this.onChange = fn;
-  }
-
-  /**
-   * ControlValueAccessor
-   */
-  public registerOnTouched(): void {}
-
-  /**
-   * Sets log value.
-   * @param lv
-   */
-  public setLogValue(lv: number): void {
-    this.log = lv;
-    this.value = Math.round(Math.pow(2, lv));
-  }
-
-  /**
-   * Sets value.
-   * @param v
-   */
-  public setValue(v: number): void {
-    this.log = Math.log2(v);
-    this.value = v;
+  constructor() {
+    super();
+    this.min = 20;
+    this.max = 20000;
+    this.step = 1;
+    this.log = true;
+    this.rangeMin = Math.log(this.min);
+    this.rangeMax = Math.log(this.max);
   }
 }
--- apps/webaudio-spectrum-analyser/src/app/components/input-range/input-range.component.ts
+++ apps/webaudio-spectrum-analyser/src/app/components/input-range/input-range.component.ts
@@ -29,9 +29,19 @@ export class InputRangeComponent implements ControlValueAccessor, OnChanges {
 
   @Input() public step = 1e-4;
 
+  @Input() public log = false;
+
   public _value = 0;
 
   public rangeName = '';
+
+  public rangeValue = 0;
+
+  public rangeMin = 0;
+
+  public rangeMax = 0;
+
+  public rangeStep = 1e-8;
 
   /**
    * Value getter.
@@ -45,10 +55,41 @@ export class InputRangeComponent implements ControlValueAccessor, OnChanges {
    */
   public set value(v: number) {
     this._value = v;
+    this.rangeValue = this.valueToRange(v);
     this.onChange(this._value);
   }
 
   private onChange = (_: any) => {};
+
+  /**
+   * TODO: description
+   * @param x
+   */
+  private rangeToValue(x: number) {
+    if (this.log) {
+      x = Math.exp(x);
+      x = Math.round(x / this.step) * this.step;
+    }
+    x = Number(String(x).replace(/0{4,}\d$/, ''));
+    return x;
+  }
+
+  /**
+   * TODO: description
+   * @param x
+   */
+  private valueToRange(x: number) {
+    return this.log ? Math.log(x) : x;
+  }
+
+  /**
+   * TODO: description
+   * @param x
+   */
+  public setRangeValue(value: number) {
+    this.value = this.rangeToValue(value);
+    this.rangeValue = value;
+  }
 
   /**
    * Lifecycle hook.
@@ -57,6 +98,22 @@ export class InputRangeComponent implements ControlValueAccessor, OnChanges {
   public ngOnChanges(changes: SimpleChanges): void {
     if (changes.name) {
       this.rangeName = `range${changes.name.currentValue}`;
+    }
+    if (changes.min || changes.log) {
+      this.rangeMin = this.valueToRange(this.min);
+    }
+    if (changes.max || changes.log) {
+      this.rangeMax = this.valueToRange(this.max);
+    }
+    if (changes.step || changes.log) {
+      if (this.log) {
+        this.rangeStep = 1e-8;
+      } else {
+        this.rangeStep = this.step;
+      }
+    }
+    if (changes.log) {
+      this.rangeValue = this.valueToRange(this.value);
     }
   }
 
