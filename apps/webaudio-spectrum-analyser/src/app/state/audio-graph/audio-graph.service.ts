f429c86bd0075238e1ebce0fd03f8cd8d2f84b11
dead-beef <dead-beef@users.noreply.github.com>
2020-09-17 22:24:43 +0000

    fix(state): reset graph state if it is invalid

--- apps/webaudio-spectrum-analyser/src/app/state/audio-graph/audio-graph.service.ts
+++ apps/webaudio-spectrum-analyser/src/app/state/audio-graph/audio-graph.service.ts
@@ -6,6 +6,7 @@ import { AudioGraph } from '../../classes/audio-graph/audio-graph';
 //import { PitchDetection, MethodOf } from '../../interfaces';
 import { PitchDetection } from '../../interfaces';
 import { AUDIO_GRAPH } from '../../utils/injection-tokens';
+import { AUDIO_GRAPH_STATE_DEFAULTS } from './audio-graph.model';
 import { audioGraphAction } from './audio-graph.actions';
 import { AudioGraphState } from './audio-graph.store';
 
@@ -25,7 +26,13 @@ export class AudioGraphService {
     const state = this.store.selectSnapshot(AudioGraphState.state);
     console.log('state', state);
     if (state !== null && state !== undefined) {
-      this.graph.setState(state);
+      try {
+        this.graph.setState(state);
+      } catch (err) {
+        console.warn('invalid state', state);
+        //this.store.reset(AUDIO_GRAPH_STATE_DEFAULTS);
+        void this.dispatch('setState', AUDIO_GRAPH_STATE_DEFAULTS);
+      }
     }
   }
 
