a9ca679c368920c077806f5a359f40c84d718668
dead-beef <dead-beef@users.noreply.github.com>
2021-11-02 06:40:07 +0000

    fix(filter): fix filter processor

--- apps/webaudio-spectrum-analyser/src/app/classes/worklet-processor/filter-processor.ts
+++ apps/webaudio-spectrum-analyser/src/app/classes/worklet-processor/filter-processor.ts
@@ -226,10 +226,10 @@ export class FilterProcessor extends AudioWorkletProcessor {
       const importObj = {
         env: {
           table: new WebAssembly.Table({
-            //initial: 2,
-            //maximum: 64,
-            initial: 0,
-            maximum: 0,
+            initial: 4,
+            maximum: 64,
+            //initial: 0,
+            //maximum: 0,
             element: 'anyfunc',
           }),
           tableBase: 0,
